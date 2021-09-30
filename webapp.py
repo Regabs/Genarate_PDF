d7f786cdb00f74095236eefaf6cac07e250946f1
Regabs <redemtusbagas@gmail.com>
2021-09-30 15:21:54 +0700

    Edit Web

--- webapp.py
+++ webapp.py
@@ -5,9 +5,14 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
-    title = "Dokumentasi WebApp"
+    title = "Beranda WebApp"
     return render_template("index.html", title = title)
+
+@app.route('/dokumentasi')
+def dokumentasi():
+    title = "Dokumentasi WebApp"
+    return render_template("dokumentasi.html", title=title)
 
 app.run()
